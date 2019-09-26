7e2d8b756d548c686643ed418b20f77fb5c0404f
Magnus Bellstrand <bellstrand@gmail.com>
2019-09-26 11:37:08 +0200

    Init

---
+++ README.md
@@ -0,0 +1,1 @@
+# GWEN React Components
---
+++ doczrc.js
@@ -0,0 +1,11 @@
+export default {
+	typescript: true,
+	title: "InsertCoin - GWEN React Components",
+	description: "",
+	typescript: true,
+	themeConfig: {
+		mode: "dark",
+	},
+	menu: ["Getting Started", "Level", "Mission", "Achievement", "Shop", "Leaderboard", "Self Reporting", "Other"],
+	ignore: ["README.md"],
+}
