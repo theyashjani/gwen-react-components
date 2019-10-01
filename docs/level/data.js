735ab9f55a86741283837234032b86bbe759cb49
Magnus Bellstrand <bellstrand@gmail.com>
2019-10-01 09:00:50 +0200

    log data

---
+++ docs/level/data.js
@@ -0,0 +1,14 @@
+export const LogData = [
+	{ type: "Review", xp: 10, date: new Date(), text: "Review a Product" },
+	{ type: "Play", xp: 10, date: new Date(), text: "Play a Game" },
+	{ type: "Like", xp: 10, date: new Date(), text: "Like a Picture" },
+	{ type: "Share", xp: 10, date: new Date(), text: "Share a Story" },
+	{ type: "Subscribe", xp: 10, date: new Date(), text: "Subscribe" },
+	{ type: "Comment", xp: 10, date: new Date(), text: "Comment a Post" },
+	{ type: "Pat", xp: 10, date: new Date(), text: "Pat a Tiger" },
+	{ type: "Love", xp: 10, date: new Date(), text: "Sharing Is Caring" },
+	{ type: "Pat", xp: 10, date: new Date(), text: "Pat a Cat" },
+	{ type: "Hug", xp: 10, date: new Date(), text: "Hug a Friend" },
+]
+
+export const RewardData = [{ currency: "coin", amount: 50 }]
