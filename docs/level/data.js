333d94495426a913458521afe7e8acd56117ad31
Magnus Bellstrand <bellstrand@gmail.com>
2019-11-04 15:55:51 +0100

    gh pages fix

--- docs/level/data.js
+++ docs/level/data.js
@@ -12,3 +12,8 @@ export const LogData = [
 ]
 
 export const RewardData = [{ currency: "coin", amount: 50 }]
+
+export const AvatarData = {
+	url: "https://storage.googleapis.com/gwen-prod-storage/avatar-packages/default/01.svg",
+	colors: { background: "#98ca9d", shirt: "#55ab59" },
+}
