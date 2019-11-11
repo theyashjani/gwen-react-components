ac06576a38562f1095bc2c8b0ca15b2060664979
Magnus Bellstrand <bellstrand@gmail.com>
2019-11-11 15:28:46 +0100

    fix docz

---
+++ docs/achievement/data.js
@@ -0,0 +1,131 @@
+export const AchievementSharer = {
+	title: "Sharing is Caring <3",
+	tiers: [
+		{
+			title: "First Time Sharer",
+			description: "Share some love with your sifnificant other! Five time's is the charm!",
+			amount: 5,
+			progress: 3,
+			icon: "https://gwen.insertcoin.se/widget/images/action-icons/FallbackPlaceholder.svg",
+			rewards: [{ currency: "xp", amount: 100 }],
+		},
+		{
+			title: "Mega Sharer",
+			description: "Everyone loves when you're a giver, share some love with everyone around you to gain some karma back! Ten time's the charm!",
+			amount: 10,
+			progress: 3,
+			icon: "https://gwen.insertcoin.se/widget/images/action-icons/FallbackPlaceholder.svg",
+			rewards: [{ currency: "coin", amount: 20 }],
+		},
+		{
+			title: "Master of Sharing",
+			description:
+				"We all love sharing, and when you share it means that you care! And to become a master of sharing you need to keep it up in your everyday life. Spread som love and become a master of sharing!",
+			amount: 25,
+			progress: 3,
+			icon: "https://gwen.insertcoin.se/widget/images/action-icons/FallbackPlaceholder.svg",
+			rewards: [
+				{ currency: "xp", amount: 500 },
+				{ currency: "coin", amount: 50 },
+			],
+		},
+	],
+}
+
+export const AchievementSuperGiver = {
+	title: "Super Giver",
+	tiers: [
+		{
+			amount: 10,
+			progress: 6,
+			icon: "https://gwen.insertcoin.se/widget/images/action-icons/FallbackPlaceholder.svg",
+		},
+	],
+}
+
+export const AchievementSharingIsCaring = {
+	title: "Sharing is Caring <3",
+	tiers: [
+		{
+			amount: 1,
+			progress: 1,
+			icon: "https://gwen.insertcoin.se/widget/images/action-icons/FallbackPlaceholder.svg",
+			completed: new Date(),
+		},
+	],
+}
+
+export const Achievements = [
+	{
+		title: "Super Giver",
+		tiers: [
+			{
+				amount: 10,
+				progress: 6,
+				icon: "https://gwen.insertcoin.se/widget/images/action-icons/FallbackPlaceholder.svg",
+			},
+		],
+	},
+	{
+		title: "Sharing is Caring <3",
+		tiers: [
+			{
+				amount: 1,
+				progress: 1,
+				icon: "https://gwen.insertcoin.se/widget/images/action-icons/FallbackPlaceholder.svg",
+				completed: new Date(),
+			},
+		],
+	},
+	{
+		title: "Sweet Symphony",
+		tiers: [
+			{
+				amount: 15,
+				progress: 3,
+				icon: "https://gwen.insertcoin.se/widget/images/action-icons/FallbackPlaceholder.svg",
+			},
+		],
+	},
+	{
+		title: "Coding",
+		tiers: [
+			{
+				amount: 5,
+				progress: 3,
+				icon: "https://gwen.insertcoin.se/widget/images/action-icons/FallbackPlaceholder.svg",
+			},
+		],
+	},
+	{
+		title: "Spint the Bottle",
+		tiers: [
+			{
+				amount: 5,
+				progress: 4,
+				icon: "https://gwen.insertcoin.se/widget/images/action-icons/FallbackPlaceholder.svg",
+			},
+		],
+	},
+	{
+		title: "Food Run",
+		tiers: [
+			{
+				amount: 3,
+				progress: 3,
+				icon: "https://gwen.insertcoin.se/widget/images/action-icons/FallbackPlaceholder.svg",
+				completed: new Date(),
+			},
+		],
+	},
+	{
+		title: "Poke a Bear",
+		tiers: [
+			{
+				amount: 1,
+				progress: 0,
+				icon: "https://gwen.insertcoin.se/widget/images/action-icons/FallbackPlaceholder.svg",
+			},
+		],
+	},
+]
