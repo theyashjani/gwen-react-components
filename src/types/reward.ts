4bb0b61e2e99f453712e173912963828d69d39e4
Magnus Bellstrand <bellstrand@gmail.com>
2020-02-13 12:15:41 +0100

    update worflow test run

--- src/types/reward.ts
+++ src/types/reward.ts
@@ -8,19 +8,11 @@ export enum UserCurrencyEnum {
 	"coin",
 	"vipcurrency",
 }
-
 export type UserCurrency = keyof typeof UserCurrencyEnum
-
-export enum CurrencyEnum {
-	"xp",
-	"coin",
-	"vipcurrency",
-}
 
 export enum TeamCurrencyEnum {
 	"team-xp",
 }
-
 export type TeamCurrency = keyof typeof TeamCurrencyEnum
 
 export type Currency = UserCurrency | TeamCurrency
