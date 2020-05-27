a19962b2fd61a4e57e7da303d7f333dd33af101e
Oskar Willman <oskar@insertcoin.se>
2020-05-27 10:22:17 +0200

    stuff

--- src/skills/tier-bar.tsx
+++ src/skills/tier-bar.tsx
@@ -3,21 +3,35 @@ import styled from "styled-components"
 import { TierIcon, TierIconShape } from "./tier-icon"
 
 interface Props {
-	currentProgress: number
-	requiredProgress: number
-	tierNumber: number
-	maxTier?: number
+	currentTierNumber: number
+	currentTierProgress: number
+	currentTierTotal: number
+	max?: number
 	icon: TierIconShape
 	iconColor?: string
-	foregroundColor?: string
+	progressColor?: string
 	backgroundColor?: string
-	textColor?: string
 }
 
 export const TieredProgressBar = (props: Props) => {
+	const { currentTierNumber, currentTierProgress, currentTierTotal, max, progressColor, backgroundColor } = props
 	return (
 		<Wrapper>
-			<TierIcon text={props.tierNumber} type={props.icon} />
+			<TierIcon text={currentTierNumber} type={props.icon} color={props.iconColor} />
+			{(() => {
+				if (!max) return <Tier color={progressColor} background={backgroundColor} progress={Math.floor((currentTierProgress / currentTierTotal) * 100)} />
+				const items: JSX.Element[] = []
+				for (let i = 1; i <= max; i++) {
+					if (i < currentTierNumber) {
+						items.push(<Tier color={progressColor} background={backgroundColor} progress={100} />)
+					} else if (i > currentTierNumber) {
+						items.push(<Tier color={progressColor} background={backgroundColor} progress={0} />)
+					} else {
+						items.push(<Tier color={progressColor} background={backgroundColor} progress={Math.floor((currentTierProgress / currentTierTotal) * 100)} />)
+					}
+				}
+				return items
+			})()}
 		</Wrapper>
 	)
 }
@@ -28,8 +42,8 @@ const Wrapper = styled.div`
 `
 
 interface TierProps {
+	progress: number
 	color?: string
-	progress?: number
 	background?: string
 }
 
@@ -39,9 +53,10 @@ const Tier = styled.div`
 	&:last-child {
 		border-radius: 0 50% 50% 0;
 	}
-	background: ${(props: TierProps) =>
-		props.progress
-			? `linear-gradient(90deg, ${props.color || "green"}, ${props.progress}, ${props.background || "grey"}, ${100 - props.progress})`
-			: props.color || "grey"};
+	background: ${(props: TierProps) => {
+		if (props.progress === 100) return props.color || "green"
+		if (props.progress === 0) return props.background || "grey"
+		return `linear-gradient(90deg, ${props.color || "green"}, ${props.progress}%, ${props.background || "grey"}, ${100 - props.progress}%))`
+	}};
 	margin-left: 5px;
 `
